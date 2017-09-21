db6a66c2a003029475a1632b628a044d09abcf9d
davit-abovyan <davit.abovyan@gmail.com>
2017-09-21 19:56:23 +0400

    API controller added

---
+++ src/main/java/am/vector/api/APIController.java
@@ -0,0 +1,17 @@
+package am.vector.api;
+
+import org.apache.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1.0/")
+public class APIController {
+    protected Logger LOGGER = Logger.getLogger(APIController.class);
+
+    public ResponseEntity getEmployeesList(){
+        return new ResponseEntity("The list", HttpStatus.OK);
+    }
+}
